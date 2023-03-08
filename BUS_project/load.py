f7c0cd0db6743c2c34bde808b6d21ef99e1b7670
Geon Choi <choigeon@postech.ac.kr>
2023-03-08 00:49:51 +0000

    second commit

--- BUS_project/load.py
+++ BUS_project/load.py
@@ -35,11 +35,26 @@ print(f"Image dimensions: {image_width} x {image_height}")
 print(f"Label names: {class_names}")
 print(f"Label counts: {num_each}")
 
-########################################################
+import numpy as np
+import matplotlib.pyplot as plt
+import PIL
+
+plt.subplots(3, 3, figsize=(8, 8))
+for i, k in enumerate(np.random.randint(num_total, size=9)):
+    im = PIL.Image.open(image_files_list[k])
+    arr = np.array(im)
+    plt.subplot(3, 3, i + 1)
+    plt.xlabel(class_names[image_class[k]])
+    plt.imshow(arr, cmap="gray", vmin=0, vmax=255)
+plt.tight_layout()
+plt.show()
+print("Hello") 
+
 # define transforms for image and segmentation
 images = image_files_list
 segs = seg_files_list
 
+import torch
 import monai
 from monai.data import ArrayDataset, create_test_image_2d, decollate_batch, DataLoader
 from monai.inferers import sliding_window_inference
@@ -54,6 +69,8 @@ from monai.transforms import (
     ScaleIntensity,
 )
 from monai.visualize import plot_2d_or_3d_image
+
+from torch.utils.tensorboard import SummaryWriter
 
 train_imtrans = Compose(
     [
@@ -71,7 +88,48 @@ train_segtrans = Compose(
         RandRotate90(prob=0.5, spatial_axes=(0, 1)),
     ]
 )
+
 val_imtrans = Compose([LoadImage(image_only=True, ensure_channel_first=True), ScaleIntensity()])
-val_segtrans = Compose([LoadImage(image_only=True, ensure_channel_first=True), ScaleIntensity()])
+val_segtrans = Compose([LoadImage(image_only=True, ensure_channel_first=True), ScaleIntensity()]) #  validation에서는 crop, rotate를 하지 않는다.
+
 # define array dataset, data loader
 check_ds = ArrayDataset(images, train_imtrans, segs, train_segtrans)
+check_loader = DataLoader(check_ds, batch_size=10, num_workers=2, pin_memory=torch.cuda.is_available())
+im, seg = monai.utils.misc.first(check_loader)
+print(im.shape, seg.shape)
+print("Hello")
+
+# shuffle
+val_frac = 0.15
+test_frac = 0.15
+length = len(images)
+indices = np.arange(length)
+np.random.shuffle(indices)
+
+images = [images[i] for i in indices] ### shuffled list
+segs = [segs[i] for i in indices] ### shuffled list
+
+test_split = int(test_frac * length)
+val_split = int(val_frac * length) + test_split
+test_indices = indices[:test_split]
+val_indices = indices[test_split:val_split]
+train_indices = indices[val_split:]
+
+
+#train_x = [image_files_list[i] for i in train_indices]
+#train_y = [image_class[i] for i in train_indices]
+#val_x = [image_files_list[i] for i in val_indices]
+#val_y = [image_class[i] for i in val_indices]
+#test_x = [image_files_list[i] for i in test_indices]
+#test_y = [image_class[i] for i in test_indices]
+
+print(f"Training count: {len(train_indices)}, Validation count: " f"{len(val_indices)}, Test count: {len(test_indices)}")
+
+# create a training data loader --> 그 전에 섞어야 할 것 같은데...
+train_ds = ArrayDataset(images[:20], train_imtrans, segs[:20], train_segtrans)
+train_loader = DataLoader(train_ds, batch_size=4, shuffle=True, num_workers=8, pin_memory=torch.cuda.is_available())
+# create a validation data loader
+val_ds = ArrayDataset(images[-20:], val_imtrans, segs[-20:], val_segtrans)
+val_loader = DataLoader(val_ds, batch_size=1, num_workers=4, pin_memory=torch.cuda.is_available())
+dice_metric = DiceMetric(include_background=True, reduction="mean", get_not_nans=False)
+post_trans = Compose([Activations(sigmoid=True), AsDiscrete(threshold=0.5)])
