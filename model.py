0ab183b65cd1858c7f86d699967fcfbdf66b83f8
Geon <choigeon@postech.ac.kr>
2023-03-31 17:56:01 +0900

    model.py update

--- model.py
+++ model.py
@@ -21,52 +21,33 @@ class Up_SMART_Net(Multi_Task_Model):
     def __init__(
         self,
         encoder_name: str = "UNet",
-        #encoder_depth: int = 5,
-        decoder_use_batchnorm: bool = True,
-        decoder_channels: List[int] = (1024, 512, 512, 256, 256, 128, 128, 64, 64), # decoder가 두 개라...
-        decoder_attention_type: Optional[str] = None,
-        in_channels: int = 1,
     ):
         super().__init__()
-
+        
         self.encoder = UNetEncoder()
 
         # CLS
         self.classification_head = ClassificationHead(
-            in_channels=self.encoder.out_channels[-1], 
+            in_channels=1024, 
             pooling='avg', 
             dropout=0.5,
-            out_channels=2, ####
+            out_channels=2, 
         )
 
         # SEG
-        self.seg_decoder = UNetDecoder(
-            encoder_channels=self.encoder.out_channels,  # [1, 64, 64, 128, 128, 256, 256, 512, 512, 1024]
-            decoder_channels=decoder_channels, # [1024, 512, 512, 256, 256, 128, 128, 64, 64]
-            use_batchnorm=decoder_use_batchnorm,
-            center=False,
-            attention_type=decoder_attention_type,
-        )
+        self.seg_decoder = UNetDecoder()
         
-        self.segmentation_head = SegmentationHead(
-            in_channels = 64,
-            #in_channels=decoder_channels[-1]//2,
+        self.segmentation_head = SegmentationHead( # head 지금 사용 안 함.
+            in_channels=64,
             out_channels=1,
             kernel_size=3,
         )
 
         # REC
-        self.rec_decoder = AE_Decoder(
-            encoder_channels = [64, 128, 256, 512, 1024], # 임의로 설정 --> 최적화 필요 --> encoder out channel로 하는 듯
-            decoder_channels = [1024, 512, 256, 128], # 임의로 설정 --> 최적화 필요 --> decoder in channel로 하는 듯
-            #encoder_channels=self.encoder.out_channels, 
-            #decoder_channels=decoder_channels, 
-            center=False,
-            attention_type=decoder_attention_type,
-        )
+        self.rec_decoder = AE_Decoder()
+        
         self.reconstruction_head = ReconstructionHead(
             in_channels=64,
-            #in_channels=decoder_channels[-1]//2,
             out_channels=1,
             kernel_size=3,
         )
