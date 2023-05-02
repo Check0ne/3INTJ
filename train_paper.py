b5b14b453c6d2e61fe0c2787dd32170a2f2dcaae
Geon Choi <choigeon@postech.ac.kr>
2023-05-02 17:25:27 +0900

    code optimization

--- train_paper.py
+++ train_paper.py
@@ -1,3 +1,4 @@
+#%%
 import torch
 import torch.nn.functional as F
 import os
@@ -17,102 +18,28 @@ from optimizers import create_optim
 from lr_schedulers import create_scheduler
 
 # data load
-data_dir = '/workspace/Dataset_BUSI/Dataset_BUSI_with_GT'
+data_dir = '/workspace/Dataset_BUSI_preprocessed'
 
 class_names = ['benign', 'malignant']
-#class_names = ['benign', 'malignant', 'normal']
 num_class = len(class_names)
 
-image_files = [
+images_dir = [
     sorted(glob(os.path.join(data_dir, class_names[i], "*).png")))
     for i in range (num_class)
 ]
 
-seg_files = [
-    sorted(glob(os.path.join(data_dir, class_names[i], "*mask*.png"))) # mask가 여러 개인 image의 경우, 첫 번째 mask만 불러오고 있다. --> mask 병합이 필요하다.
+masks_dir = [
+    sorted(glob(os.path.join(data_dir, class_names[i], "*mask.png"))) 
     for i in range (num_class)
 ]
 
-new_seg_files = [
-    sorted(glob(os.path.join(data_dir, class_names[i], "*mask.png"))) # mask가 여러 개인 image의 경우, 첫 번째 mask만 불러오고 있다. --> mask 병합이 필요하다.
-    for i in range (num_class)
-]
+num_each = [len(images_dir[i]) for i in range(num_class)] # class 별 image_files 개수 리스트
+images_class = [0 for i in range(num_each[0])] + [1 for i in range(num_each[1])]
+images_dir = images_dir[0] + images_dir[1]
+masks_dir = masks_dir[0] + masks_dir[1]
 
-# print(len(image_files[0]), len(image_files[1]), len(image_files[2]))
-print(len(image_files[0]), len(image_files[1]))
+print(f"Benign: {num_each[0]}, Malignant: {num_each[1]}")
 
-num_each = [len(image_files[i]) for i in range(num_class)] # class 별 image_files 개수 리스트
-
-image_files_list = [] 
-seg_files_list = []
-new_seg_files_list = []
-image_class = [] # benign --> 0, malignant --> 1
-
-for i in range(num_class): 
-    image_files_list.extend(image_files[i]) # image_files[i] 리스트의 요소(image)를 image_files_list에 추가
-    seg_files_list.extend(seg_files[i])
-    new_seg_files_list.extend(new_seg_files[i])
-    image_class.extend([np.array(i)] * num_each[i]) # class 이름 리스트 생성 [A, A, A, ... B, B, B, ... C, C, C ...] class 요소의 개수는 num_each 
-num_total = len(image_class) # image_class의 요소 개수
-image_width, image_height = Image.open(image_files_list[0]).size # 하나의 이미지 열어서 width, height 값 추출
-
-print(f"Total image count: {num_total}")
-print(f"Image dimensions: {image_width} x {image_height}")
-print(f"Label names: {class_names}")
-print(f"Label counts: {num_each}")
-
-# normal_mask만 numpy arry가 3차원이다. 기존의 channel=3을 없애고 다시 channel=1을 추가했다. transform까지 정상적으로 되는 것을 확인할 수 있다.
-# 기존에는 MONAI에서 파일명으로 접근했지만, 배열을 수정해야 하기 때문에 파일을 열어서 배열을 수정하고, 수정한 배열을 리스트에 넣는다.
-images = [np.expand_dims(np.array(PIL.Image.open(i))[:, :, 0], axis=2) for i in image_files_list]
-
-segs = [] # mask numpy array before merging
-
-for i in seg_files_list:
-    if np.array(PIL.Image.open(i)).ndim == 2:
-        segs.append(np.expand_dims(np.array(PIL.Image.open(i)), axis=2))
-    else:
-        segs.append(np.expand_dims(np.array(PIL.Image.open(i))[:, :, 0], axis=2))
-
-print(len(segs)) # 798
-
-new_segs = [] # mask numpy array after merging
-
-for i in range(len(segs)):
-    if i == 0:
-        temp_dir = seg_files_list[i]
-        temp_arr = segs[i]
-    else:
-        new_dir = seg_files_list[i]
-        new_arr = segs[i]
-        if temp_dir.replace('.png','') in new_dir:
-            temp_arr = temp_arr | new_arr
-        else:
-            new_segs.append(temp_arr)
-            temp_dir = new_dir
-            temp_arr = new_arr
-    if i == len(segs)-1:
-        new_segs.append(temp_arr)              
-            
-print(len(new_segs)) # 780
-
-
-images = images[227:] # for downsampling
-new_segs= new_segs[227:] # for downsampling
-image_class = image_class[227:] # for downsampling
-print(f"Case counts after down sampling: {len(images)}")
-
-
-m = 2
-print(new_seg_files_list[m])
-plt.imshow(new_segs[m]*255, cmap = "gray", vmin=0, vmax=255)
-plt.tight_layout()
-plt.show()
-
-#%%
-# new_seg_files_list는 segmentation mask 파일 이름으로 이루어진 list (총 780개)
-# new_seg는 segmentation mask array로 이루어진 list (True, False, 총 780개, 병합 완료)
-
-# define transforms for image and segmentation
 import torch
 import monai
 from monai.data import ArrayDataset, create_test_image_2d, decollate_batch, DataLoader
@@ -129,85 +56,75 @@ from monai.transforms import (
     SaveImage,
     Resize,
     Lambda,
+    EnsureChannelFirst,
+    RandRotate90,
+    RandFlip,
+    Rotate90,
+    Flip,
 )
 from monai.visualize import plot_2d_or_3d_image
 from torch.utils.tensorboard import SummaryWriter
 
-resize_h = 224 # UNet에서 maxpoolking을 4번 실행하기 때문에 crop_size를 16의 배수로 실행한다. --> 데이터 손실이 발생하므로 crop 말고 resize로 해보자.
+resize_h = 224
 resize_w = 224
-
-train_imtrans = Compose(
-    [
-        #LoadImage(image_only=True, ensure_channel_first=True), # numpy 배열 그대로 넣어주기 때문에 load하지 않는다.
-        #EnsureChannelFirst(),
-        AsChannelFirst(), # shape가 (, , channel)이다. MONAI framework의 transform에서는 (channel, , ) shape를 가정하므로 변환한다.
-        #RemoveRepeatedChannel(repeats=3), # 중복되는 channe=3을 channel=1로 줄인다.
-        Resize((resize_h,resize_w), mode='area'),
-        ScaleIntensity(),
-    ]
-)
-train_segtrans = Compose(
-    [
-        #LoadImage(image_only=True, ensure_channel_first=True),
-        AsChannelFirst(),
-        Resize((resize_h,resize_w), mode='area'),
-        ScaleIntensity(),
-    ]
-)
-
 
 def int_to_tensor(x):
     return torch.tensor(x, dtype=float).reshape((1,))
 
+train_imtrans = Compose(
+        [   
+            LoadImage(image_only=True, ensure_channel_first=True, dtype=np.float32), 
+            ScaleIntensity(),
+            Rotate90(spatial_axes=(0, 1)),
+            Flip(spatial_axis=0),
+            Resize((resize_h,resize_w), mode='area'),
+            
+            # data augmentation
+        ]
+    )
+
+train_segtrans = Compose(
+    [
+        LoadImage(image_only=True, ensure_channel_first=True, dtype=np.float32),
+        Rotate90(spatial_axes=(0, 1)),
+        Flip(spatial_axis=0),
+        Resize((resize_h,resize_w), mode='area'),
+    ]
+)
 
 clstrans = Compose([
     Lambda(int_to_tensor)
 ])
 
+'''
+import matplotlib.pyplot as plt
+from torchvision.transforms import ToPILImage
 
-print(images[0].shape)
-ds = ArrayDataset(images, train_imtrans) 
-print(type(ds))
-print(ds[0].shape) # channel이 맨 앞에 있는 tensor로 변환.
+# ToPILImage 객체 생성
+to_pil = ToPILImage()
 
-val_imtrans = Compose([AsChannelFirst(), Resize((resize_h, resize_w), mode='area'), ScaleIntensity()])
-val_segtrans = Compose([AsChannelFirst(), Resize((resize_h, resize_w), mode='area'), ScaleIntensity()]) #  validation에서는 crop, rotate를 하지 않는다. --> Check
+# 텐서를 PIL 이미지로 변환
+pil_im = to_pil(check_ds[0][1])
 
-test_imtrans = Compose([AsChannelFirst(), Resize((resize_h, resize_w), mode='area'), ScaleIntensity()])
-test_orgtrans = Compose([AsChannelFirst(), ScaleIntensity()])
-test_segtrans = Compose([AsChannelFirst(), ScaleIntensity()]) #  test에서는 crop, rotate를 하지 않는다. --> Check
+# 이미지 출력
+plt.imshow(pil_im, cmap='gray')
+plt.show()
+'''
+
+val_imtrans = train_imtrans
+val_segtrans = train_segtrans
+
+test_imtrans = train_imtrans
+test_segtrans = train_segtrans
 
 # define array dataset, data loader
-check_ds = ArrayDataset(images, train_imtrans, new_segs, train_segtrans, image_class, clstrans) 
+check_ds = ArrayDataset(images_dir, train_imtrans, masks_dir, train_segtrans, images_class, clstrans) 
 check_loader = DataLoader(check_ds, batch_size=10, num_workers=2, pin_memory=torch.cuda.is_available())
-im, seg, cls = monai.utils.misc.first(check_loader)
-print(im.shape, seg.shape, cls.shape)
+img, mask, cls = monai.utils.misc.first(check_loader)
+print(img.shape, mask.shape, cls.shape)
 
-'''
+######################################################################################################## 여기까지 최적화 2023.05.02
 
-'''
-
-images_b = images[:210] # for downsampling
-images_m = images[210:] # for downsampling
-new_segs_b = new_segs[:210] # for downsampling
-new_segs_m = new_segs[210:] # for downsampling
-image_class_b = image_class[:210] # for downsampling
-image_class_m = image_class[210:] # for downsampling
-
-length = int(len(images)*0.5)
-
-# shuffle benign
-indices = np.arange(length)
-np.random.shuffle(indices)
-images_b = [images_b[i] for i in indices] # shuffled list
-new_segs_b = [new_segs_b[i] for i in indices] # shuffled list
-image_class_b = [image_class_b[i] for i in indices] # shuffeld list
-
-# shuffle malignant
-np.random.shuffle(indices)
-images_m = [images_m[i] for i in indices] # shuffled list
-new_segs_m = [new_segs_m[i] for i in indices] # shuffled list
-image_class_m = [image_class_m[i] for i in indices] # shuffeld list
 
 val_frac = 0.1
 test_frac = 0.1
